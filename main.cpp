ea58c12e53f864eb69646270e2ef453b7690776d
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-09-21 11:40:15 +0200

    Hibák felismerése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //Szintaktikai hiba
+    std::cout << '1-100 ertekek duplazasa' //Nincs ";" és "" kell a '' helyett
+    for (int i = 0;) //Helytelen for ciklus, for(int i = 0; i < N_ELEMENTS; ++i)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //Helytelen for ciklus, for(int i = 0; i < N_ELEMENTS; ++i)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //Nincs érték meghatározva, std::cout << "Ertek: " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //Nincs inicializálás
+    for (int i = 0; i < N_ELEMENTS, i++) //Helytelen for ciklus, for(int i = 0; i < N_ELEMENTS; ++i);
     {
-        atlag += b[i]
+        atlag += b[i] //Nincs ";"
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
