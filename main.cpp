ece91b72ef737b6f354a8ec87925b53dd2260bfe
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-09-21 11:43:27 +0200

    Hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //Szintaktikai hiba
-    std::cout << '1-100 ertekek duplazasa' //Nincs ";" és "" kell a '' helyett
-    for (int i = 0;) //Helytelen for ciklus, for(int i = 0; i < N_ELEMENTS; ++i)
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << "1-100 ertekek duplazasa" << std::endl; 
+    for (int i = 0; i < N_ELEMENTS; ++i)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //Helytelen for ciklus, for(int i = 0; i < N_ELEMENTS; ++i)
+    for (int i = 0; i < N_ELEMENTS; ++i) 
     {
-        std::cout << "Ertek:" //Nincs érték meghatározva, std::cout << "Ertek: " << b[i] << std::endl;
+        std::cout << "Ertek:" << b[i] << std::endl; 
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //Nincs inicializálás
-    for (int i = 0; i < N_ELEMENTS, i++) //Helytelen for ciklus, for(int i = 0; i < N_ELEMENTS; ++i);
+    int atlag = 0; 
+    for (int i = 0; i < N_ELEMENTS; ++i) 
     {
-        atlag += b[i] //Nincs ";"
+        atlag += b[i]; 
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
